a503109ff7df1937a6d03f5e89b7b41b3215bc5c
Suanne Chiang <108243961+chiangsuanne@users.noreply.github.com>
2023-12-09 14:42:28 -0800

    Update and rename query.sql to Monthly Product Revenue.sql

---
+++ Product Analytics/Monthly Product Revenue.sql
@@ -0,0 +1,22 @@
+-- Calculating descriptive statistics for monthly revenue by product	  
+
+WITH product_rev as (
+SELECT 
+    ProductName,
+    sum(Revenue) as monthly_rev,
+    date_trunc('month', OrderDate) as order_month
+FROM Products as p
+JOIN Subscriptions as s
+WHERE p.PRODUCTID = s.PRODUCTID
+    AND date_trunc('month', OrderDate) BETWEEN '2022-01-01' AND '2022-12-01'
+GROUP BY ProductName, order_month
+)
+
+SELECT
+    ProductName,
+    min(monthly_rev) as min_rev,
+    max(monthly_rev) as max_rev,
+    avg(monthly_rev) as avg_rev,
+    stddev(monthly_rev) as std_dev_rev
+FROM product_rev
+GROUP BY ProductName;
